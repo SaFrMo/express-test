16d1cb8392f463b9696a6233804052b9214f3980
Sander Moolin <sandermoolin@gmail.com>
2017-10-14 08:01:02 -0400

    Initial

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Running!')
+})
+
+app.listen(3000, '0.0.0.0', function () {
+  console.log('Example app listening on port 3000!')
+})
